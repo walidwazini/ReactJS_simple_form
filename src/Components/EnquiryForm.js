846622e49f71afb47f9e181a310c66321e9938d5
Walid Wazini <wazini25@gmail.com>
2022-01-18 14:39:18 +0800

    first commit

---
+++ src/Components/EnquiryForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const EnquiryForm = () => {
+  const [userInput, setUserInput] = useState({
+    enteredName: '',
+    enteredTopic: '',
+    enteredQuestion: ''
+  })
+
+  return (
+    <React.Fragment>
+      <form></form>
+    </React.Fragment>
+  )
+}
+
+export default EnquiryForm
