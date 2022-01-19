f2751974f86575b314d00a38c0c6f6480cf63a27
Walid Wazini <wazini25@gmail.com>
2022-01-19 10:26:51 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 
 import React from 'react';
 import './App.css';
-import EnquiryForm from './Components/EnquiryForm';
+// import EnquiryForm from './Components/EnquiryForm';
 
 function App() {
   return (
-    <div className='App'>
-      <header>
-        Header
-      </header>
-      <body>
-        Simple Form Example body
-        <div className='container' >
-          Container
-          <EnquiryForm />
+    <div>
+      <form>
+        <h1>Login Form</h1>
+        <div className='ui divider' ></div>
+        <div className='ui form'>
+          <div className='field'>
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+            // value={formValues.username}
+            // onChange={handleChange}
+            />
+          </div>
+          {/* <p>{formErrors.username}</p> */}
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+            // value={formValues.email}
+            // onChange={handleChange}
+            />
+          </div>
+          {/* <p>{formErrors.email}</p> */}
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+            // value={formValues.password}
+            // onChange={handleChange}
+            />
+          </div>
+          {/* <p>{formErrors.password}</p> */}
+          <button className="fluid ui button blue">Submit</button>
         </div>
-      </body>
+      </form>
     </div>
   );
 }
