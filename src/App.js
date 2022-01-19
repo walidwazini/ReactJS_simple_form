7bad637c12d42cfe8f4242e224e5607090ca1f60
Walid Wazini <wazini25@gmail.com>
2022-01-19 11:23:51 +0800

    validation and errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 // import EnquiryForm from './Components/EnquiryForm';
 
 function App() {
+  const initialValues = { username: "", email: "", password: "" };
+  const [formValues, setFormValue] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = ev => {
+    // console.log(ev.target)
+    const { name, value } = ev.target
+    setFormValue({ ...formValues, [name]: value })
+    console.log(formValues)
+  }
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    // Will get errors object from validate()
+    setFormErrors(validate(formValues))
+    setIsSubmit(true)
+  }
+
+  useEffect(() => {
+    
+  } , [formErrors])
+
+  const validate = (val) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    if (!val.username) {
+      errors.username = 'Username is required'
+    }
+    if (!val.email) {
+      errors.email = 'Username is required'
+    }
+    if (!val.password) {
+      errors.password = 'Username is required'
+    }
+    // This erros will be send into setFormErrors
+    return errors
+  }
+
   return (
-    <div>
-      <form>
+    <div className='container'>
+      <pre> {JSON.stringify(formValues, undefined, 2)} </pre>
+      <form onSubmit={handleSubmit} >
         <h1>Login Form</h1>
         <div className='ui divider' ></div>
         <div className='ui form'>
@@ -16,8 +56,8 @@ function App() {
               type="text"
               name="username"
               placeholder="Username"
-            // value={formValues.username}
-            // onChange={handleChange}
+              value={formValues.username}
+              onChange={handleChange}
             />
           </div>
           {/* <p>{formErrors.username}</p> */}
@@ -27,8 +67,8 @@ function App() {
               type="text"
               name="email"
               placeholder="Email"
-            // value={formValues.email}
-            // onChange={handleChange}
+              value={formValues.email}
+              onChange={handleChange}
             />
           </div>
           {/* <p>{formErrors.email}</p> */}
@@ -38,8 +78,8 @@ function App() {
               type="password"
               name="password"
               placeholder="Password"
-            // value={formValues.password}
-            // onChange={handleChange}
+              value={formValues.password}
+              onChange={handleChange}
             />
           </div>
           {/* <p>{formErrors.password}</p> */}
